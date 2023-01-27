da1a6b0b062a23216bf35285d29200c3b30498a3
Your Name <JulienneJulien15@gmail.com>
2023-01-27 16:05:19 -0500

    html routes created

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
+// HTML ROUTES CREATED
+// GET * should return the index.html file.
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+// GET /notes should return the notes.html file.
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 
 
-
-
-
+// START LISTENING
 app.listen(PORT, () => console.log('Server Listening on port' + PORT));
