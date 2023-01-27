0685eeea63bcefbca3a45aab6cb030b4d9f0b3b5
Your Name <JulienneJulien15@gmail.com>
2023-01-27 15:57:18 -0500

    set port

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
 const path = require("path");
 const fs = require("fs");
+const uuid = require("uuid");
+const notes = require("./db/db.json");
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log('Server Listening on port' + PORT));
