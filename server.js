856ce913aadd62a3841c9d0985f700ef279fafd8
Your Name <JulienneJulien15@gmail.com>
2023-01-27 16:15:35 -0500

    api routes added/npm package uuid added for unique id can be given

--- server.js
+++ server.js
@@ -21,7 +21,19 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-
-
+// API ROUTES CREATED
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. NPM Package - UUID was added to each new note a unique id 
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
 // START LISTENING
 app.listen(PORT, () => console.log('Server Listening on port' + PORT));
